df76371c0b2e20eaa6f4ec7c46d2f7ce7e657a39
Brigitte Heijkoop <112940131+BrigitteHx@users.noreply.github.com>
2022-09-07 10:06:02 +0200

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+# hello.py
+
+print('Hello wereld van...')
+
+print('pppp    y     y   ttttttt   h   h    oooo   nnnn') 
+print('p   p    y   y       t      h   h   o    o  n   n')
+print('p   p     y y        t      h   h   o    o  n   n') 
+
+print('p   p     y y        t      h   h   o    o  n   n')
+print('pppp       y         t      hhhhh   o    o  n   n')
+print('p          y         t      h   h   o    o  n   n')
+print('p          y         t      h   h   o    o  n   n')
+print('p          y         t      h   h    oooo   n   n')
+
+print('Brigitte Heijkoop 99073629')
+
