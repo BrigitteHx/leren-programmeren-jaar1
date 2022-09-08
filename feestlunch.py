626555f308901bbe0b62a4ada09ce4dcc3d4d7d3
BrigitteHx <99073629@mydavinci.nl>
2022-09-08 11:15:25 +0200

    feestlunch update

--- feestlunch.py
+++ feestlunch.py
@@ -5,4 +5,4 @@ y = stokbrood = float(2.78)
 z = korting = float(0.50)
 
 lunch = 17 * x + 2 * y - 3 * z 
-print(float(lunch))
+print('De feestlunch kost je bij de bakker', float(lunch),'euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
