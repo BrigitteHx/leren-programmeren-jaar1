36ec019eb3430c8dcdde7c7345f518d97867b757
BrigitteHx <99073629@mydavinci.nl>
2022-09-08 13:21:14 +0200

    update cent feestlunch en speelhal

--- dagjespeelhal.py
+++ dagjespeelhal.py
@@ -1,15 +1,15 @@
 # dagjespeelhal
 
 x = ticket = 745
-y = VIPveenmin = 37 / 5
+y = VIPveenmin = round(37 / 5)
 
 totaaltickets = int(input('Totaal Aantal Tickets: '))
 bedragtickets = totaaltickets * x
-print('Totaal bedrag tickets: ', bedragtickets)
+print('Totaal bedrag tickets: ', bedragtickets, 'cent')
 
 totaaltijdVIP = int(input('Totale Min in VIP-VR-GameSeat: '))
 bedragtijdVIP = totaaltijdVIP * y
-print('Totaal VIP-VR-GameSeat', bedragtijdVIP)
+print('Totaal VIP-VR-GameSeat', bedragtijdVIP, 'cent') 
 
 totalekosten = bedragtickets + bedragtijdVIP
-print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar', {totalekosten} , 'cent!' )
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar', totalekosten , 'cent!' )
