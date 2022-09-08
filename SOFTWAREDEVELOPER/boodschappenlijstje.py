4ffede4743f1686d08f1c5da9125fb9b2257e101
BrigitteHx <99073629@mydavinci.nl>
2022-09-08 10:08:36 +0200

    feestlunch

---
+++ SOFTWAREDEVELOPER/boodschappenlijstje.py
@@ -0,0 +1,10 @@
+# boodschappenlijstje
+
+print("boodschappenlijstje voor de feestlunch")
+
+x = float(0.39)
+y = float(2.78)
+z = float(0.50)
+
+aantalcros = int(input('aantal crossantjes:'))
+hoeveelheidcros = aantalcros * x 
---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+# feestlunch
+
+x = croissant = float(0.39)
+y = stokbrood = float(2.78)
+z = korting = float(0.50)
+
+lunch = 17 * x + 2 * y - 3 * z 
+print(float(lunch))
