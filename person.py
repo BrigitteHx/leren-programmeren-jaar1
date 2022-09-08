2b7b22eebc990dad2342af36354cbde0edd4fc59
BrigitteHx <99073629@mydavinci.nl>
2022-09-08 11:48:25 +0200

    personpy added

---
+++ person.py
@@ -0,0 +1,8 @@
+# person 
+
+print('  ---------------------------------------------------------------------')
+Naam = (input(' | Naam          : '))
+Adres = (input(' | Adres         : '))
+Postcode = (input(' | Postcode      : '))
+Woonplaats = (input(' | Woonplaats    : '))
+print('  ---------------------------------------------------------------------')
