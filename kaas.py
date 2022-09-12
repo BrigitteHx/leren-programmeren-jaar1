28f56988b8f867927e78beb09f1f669c66105256
BrigitteHx <99073629@mydavinci.nl>
2022-09-12 17:19:44 +0200

    Kaas

--- kaas.py
+++ kaas.py
@@ -1,28 +1,35 @@
 # kaas
 
-while True :
-    vraag1 = input("Is de kaas geel?")
-    if vraag1 in ["Yes", "No"] : 
-        break
+vraag1 = input("Is de kaas geel? (Ja/Nee)")
+if vraag1 == "Nee":
+    vraag2 = input("Heeft de kaas blauwe schimmel? (Ja/Nee)")
+    if vraag2 == "Nee": 
+        vraag3 = input("Heeft de kaas een korst? (Ja/Nee)")
+        if vraag3 == "Nee" : 
+            print("Mozzarella")
+        else : print("Camembert")
+    else : 
+        vraag4 = input ("Heeft de Kaas een korst ?(Ja/Nee)")
+        if vraag4 == "Ja" : 
+            print("Blue de Rochbaron")
+        else : 
+            print("Foume dÁmbert")
+if vraag1 == "Ja" :
+    vraag5 = input("Zitten er gaten in? (Ja/Nee)")
+    if vraag5 == "Ja" :
+        vraag6 = input("Is de kaas belachelijk duur? (Ja/Nee)") 
+        if vraag6 == "Ja " :
+            print("Emmenthaler")
+        else :
+            print("Leerdammer")
+    else :
+        vraag7 = input("Is de kaas hard als steen? (Ja/Nee)")
+        if vraag7 == "Ja" :
+            print("Parmigiano Reggiano")
+        else : 
+            print("Goudse Kaas")
 
-if vraag1 == "Yes" :
-    vraag2 = input("Zitten er gaten in?")
-
-if vraag2 == "No" : 
-    vraag3 = input("Is de kaas hard als steen?")
-elif vraag2 == "Yes" : 
-    vraag4 = input("Is de kaas belagelijk duur?")
-
-if vraag3 == "Yes" : 
-    print("Parmigiano")
-elif vraag3 == "No" :
-    print("Goudse Kaas")
-
-if vraag4 == "Yes" : 
-    print("Emmenthaler")
-elif vraag4 == "No" : 
-    print("Leerdammer")
+       
 
 
-
-
+        
---
+++ sollicitatie.py
@@ -0,0 +1,11 @@
+#sollicitatie 
+
+while True :
+    print("Welkom bij de sollicitatie Circusdirecteur voor Circus HotelDeBotel, er zullen u een aantal vragen steld worden om te zien of u in aanmerking komt.")
+    print("---------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    vraag1 = input("Heeft u praktijk ervaring op het gebied van \'Dressuur\', \'Jongleren\' of \'Acrobatiek\'? ")
+    if vraag1 in [ "Ja" , "Nee" ] :
+        break
+
+
