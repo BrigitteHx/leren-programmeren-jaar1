c84a36a5d34d59f490fe026712792cdb74a19da4
BrigitteHx <99073629@mydavinci.nl>
2022-09-22 10:27:22 +0200

    challange toets 2

--- # challange smartphone.py
+++ # challange smartphone.py
@@ -4,10 +4,10 @@ naamsmartphone1 = input("Wat is de naam van uw iPhone? " )
 smarthphone1 = int(input("Hoe duur is uw iPhone? "))
 
 naamsmsartphone2 = input("Wat is de naam van uw Samsung? " )
-smartphone2 = int(input("Hoe duur is uw Samsung? "))
+smartphone2 = int(input("Hoe duur is uw Samsung? ")) 
 
 if smarthphone1 > smartphone2 : 
-    print(f"De {naamsmartphone1} is het duurts, de telefoon kost: {smarthphone1} Euro ")
+    print( print(f"De {naamsmartphone1} is het duurts, de telefoon kost: {smarthphone1} Euro "))
     print(f"De {naamsmsartphone2} is het goedkoopst, de telefoon kost: {smartphone2} Euro ")
     print(f"Het advies is dus de {naamsmsartphone2} te kopen, deze is {smarthphone1 - smartphone2} Euro goedkoper. ")
 elif smarthphone1 < smartphone2 :
@@ -17,4 +17,10 @@ elif smarthphone1 < smartphone2 :
 elif smarthphone1 == smartphone2 :
     print(f"De smartphones zijn even duur: {smarthphone1} Euro ")
     print("Ons advies is om de iPhone te kopen, deze is kwalitatief beter. ")
-    
+
+
+if smarthphone1 and smartphone2 > 900 :
+   print(f"De door u gekozen smartphones: {naamsmartphone1} en {naamsmsartphone2} zijn beide boven 900 euro.")
+   print("Het advies is dus geen telefoon te kopen, ze zijn te duur.")
+
+   # laatste uitbreiding nog toevoegen 
