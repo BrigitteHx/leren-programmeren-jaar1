569b59b2506971d3972787b67798774d151c506c
BrigitteHx <99073629@mydavinci.nl>
2022-09-27 12:07:38 +0200

    robotarm3

---
+++ forever-young/robotarm-python-2021-main/robotarm-python-2021-main/copyexample3.py
@@ -0,0 +1,18 @@
+from timeit import repeat
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+robotArm.speed = (3)
+
+# Jouw python instructies zet je vanaf hier:
+
+for i in range(1,5):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
