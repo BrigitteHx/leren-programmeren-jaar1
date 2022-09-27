6fd76fdc72e54da624f325a24c99f51017c0f925
BrigitteHx <99073629@mydavinci.nl>
2022-09-27 12:56:37 +0200

    robotarm4

--- forever-young/robotarm-python-2021-main/robotarm-python-2021-main/copyexample4.py
+++ forever-young/robotarm-python-2021-main/robotarm-python-2021-main/copyexample4.py
@@ -1,7 +1,7 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 4')
-robotArm.speed = (3)
+robotArm.speed = (1)
 
 # Jouw python instructies zet je vanaf hier:
 
@@ -12,6 +12,19 @@ for i in range(3):
     robotArm.drop()
     for i in range(2):
         robotArm.moveLeft()
+
+for i in range(2):
+        robotArm.moveRight()
+
+for i in range(3):
+    robotArm.grab()
+    for i in range(1):
+        robotArm.moveLeft()
+    robotArm.drop()
+    for i in range(1):
+        robotArm.moveRight()
+
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
