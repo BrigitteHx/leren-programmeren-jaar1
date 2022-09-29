9ad399fc8ae61992e6ff17e34a86d43976c0615a
BrigitteHx <99073629@mydavinci.nl>
2022-09-29 09:56:24 +0200

    a

---
+++ M03.D02.O5 - Som.py
@@ -0,0 +1,15 @@
+#M03.D02.O5 - Som > 1000
+
+
+for verandering in range (2,50):
+    verandering = 1
+
+i = 50
+som = i + verandering
+
+while i <= 999:
+    if i == 1000:
+        break
+    print(i)
+    i = som + i + verandering # nieuwe waarde voor I maken!
+print("einde")
