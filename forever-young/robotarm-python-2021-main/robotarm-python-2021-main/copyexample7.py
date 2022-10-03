3f6f51352dbd5ab554f195b3c99b54cb6de56737
BrigitteHx <99073629@mydavinci.nl>
2022-10-03 14:35:58 +0200

    7

---
+++ forever-young/robotarm-python-2021-main/robotarm-python-2021-main/copyexample7.py
@@ -0,0 +1,12 @@
+from RobotArm import RobotArm
+robotArm = RobotArm('exercise 7')
+robotArm.speed = (2)
+for i in range(5):
+    for i in range(6):
+        robotArm.moveRight()
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
+    for i in range(2):
+        robotArm.moveRight()
+robotArm.wait()
