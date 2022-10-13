8697e811475902e31301cfd5f897d51df0472f7f
BrigitteHx <99073629@mydavinci.nl>
2022-10-13 10:08:10 +0200

    stap4chalZwembad

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/ChallangesEnAantekingen/challangezwembad.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/ChallangesEnAantekingen/challangezwembad.py
@@ -1,13 +1,10 @@
 #challangezwembad
 
-#stap 1:
+# --- stap 1:
 
-from functools import total_ordering
-
-
-lengtezwembad1 = 8 #meter
-breedtezwembad1 = 3 #meter
-dieptezwembad1 = 1.5 #meter
+lengtezwembad1 = float(input("Wat is de lengte in meters? "))
+breedtezwembad1 = float(input("Wat is de breedte in meters? "))
+dieptezwembad1 = float(input("Wat is de diepte in meters? "))
 
 uitgravenperm3 = 25
 afvoerperm3 = 32.50
@@ -15,7 +12,7 @@ afvoerperm3 = 32.50
 TotaalGrondAfgevoerdZwembad1 = lengtezwembad1 * breedtezwembad1 * dieptezwembad1
 # print(f"{TotaalGrondAfgevoerdZwembad1} m3 grond in totaal. ")
 
-# --- stap 2 
+# --- stap 2: 
 
 uitgraafkosten = uitgravenperm3 * TotaalGrondAfgevoerdZwembad1
 afvoerkosten = afvoerperm3 * TotaalGrondAfgevoerdZwembad1
@@ -26,12 +23,12 @@ afvoerkosten = afvoerperm3 * TotaalGrondAfgevoerdZwembad1
 # print(f"Afvoeren grond:     € {afvoerkosten} ")
 # print(f"Totaal:             € {totaal} ")
 
-# --- stap 3
+# --- stap 3:
 
 vasteprijsklein = 100
 vasteprijsgroot = 250
 
-voorrijkostafstand = int(input("Hoeveel KM moet het worden voor gereden? "))
+voorrijkostafstand = float(input("Hoeveel KM moet het worden voor gereden? "))
 
 if voorrijkostafstand < 50 and TotaalGrondAfgevoerdZwembad1 < 20:
     voorijkosten = vasteprijsklein + voorrijkostafstand * 1.25 
@@ -44,11 +41,14 @@ elif voorrijkostafstand >= 50 and TotaalGrondAfgevoerdZwembad1 >= 20:
 
 totaal = uitgraafkosten + afvoerkosten + voorijkosten
 
+print("")
 print(f"Offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud: {TotaalGrondAfgevoerdZwembad1} m3)")
 print(f"Uitgraven:          € {uitgraafkosten}")
 print(f"Afvoeren grond:     € {afvoerkosten} ")
 print(f"Voorrijkosten:      € {voorijkosten} ")
 print(f"Totaal:             € {totaal} ")
 
+# --- stap 4:
 
+# input in stap 1 ^^ 
 
