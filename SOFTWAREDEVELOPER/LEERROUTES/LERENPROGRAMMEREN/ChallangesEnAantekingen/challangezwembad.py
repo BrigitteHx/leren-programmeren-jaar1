4b24255d9cd6d6722c83e378804fef935c2f9acc
BrigitteHx <99073629@mydavinci.nl>
2022-10-13 10:31:48 +0200

    stap5a/5bchalZwembad

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/ChallangesEnAantekingen/challangezwembad.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/ChallangesEnAantekingen/challangezwembad.py
@@ -39,16 +39,58 @@ elif voorrijkostafstand < 50 and TotaalGrondAfgevoerdZwembad1 >= 20:
 elif voorrijkostafstand >= 50 and TotaalGrondAfgevoerdZwembad1 >= 20:
     voorijkosten = vasteprijsgroot * 2.05
 
-totaal = uitgraafkosten + afvoerkosten + voorijkosten
+# totaal = uitgraafkosten + afvoerkosten + voorijkosten
+
+# print("")
+# print(f"Offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud: {TotaalGrondAfgevoerdZwembad1} m3)")
+# print(f"Uitgraven:          € {uitgraafkosten}")
+# print(f"Afvoeren grond:     € {afvoerkosten} ")
+# print(f"Voorrijkosten:      € {voorijkosten} ")
+# print(f"Totaal:             € {totaal} ")
+
+# --- stap 4:
+
+# input in stap 1 ^^ 
+
+# --- stap 5a:
+
+m2 = lengtezwembad1 * breedtezwembad1
+prijsperm2klein = 250
+prijsperm2groot = 200
+meerprijsroodklein = 25
+meerprijsroodgroot = 20
+meerprijskleurklein = 100
+meerprijskleurgroot = 125
+
+print(f"Uw zwembad is {m2} vierkante meter. ")
+kleur = input("Wilt u rood of kleuren tegels? (antwoord met rood/ kleur) ")
+
+if TotaalGrondAfgevoerdZwembad1 < 20 and kleur == "rood": 
+    prijs1 = m2 * prijsperm2klein
+    rood = m2 * meerprijsroodklein
+    totaaltegels = prijs1 + rood
+elif TotaalGrondAfgevoerdZwembad1 >= 20 and kleur == "rood": 
+    prijs1 = m2 * prijsperm2groot
+    rood = m2 * meerprijsroodgroot
+    totaaltegels = prijs1 + rood
+elif TotaalGrondAfgevoerdZwembad1 < 20 and kleur == "kleur":
+    prijs1 = m2 * prijsperm2klein
+    kleurbedrag = m2 * meerprijskleurklein
+    totaaltegels = prijs1 + kleurbedrag
+elif TotaalGrondAfgevoerdZwembad1 >= 20 and kleur == "kleur":
+    prijs1 = m2 * prijsperm2groot
+    kleurbedrag = m2 * meerprijskleurgroot 
+    totaaltegels = prijs1 * kleurbedrag
+
+# --- stap 5b 
+
+totaal = uitgraafkosten + afvoerkosten + voorijkosten + totaaltegels
 
 print("")
 print(f"Offerte voor een zwembad van 8 bij 3 bij 1.5 meter (inhoud: {TotaalGrondAfgevoerdZwembad1} m3)")
 print(f"Uitgraven:          € {uitgraafkosten}")
 print(f"Afvoeren grond:     € {afvoerkosten} ")
 print(f"Voorrijkosten:      € {voorijkosten} ")
+print(f"Beton + tegels:     € {totaaltegels} ")
 print(f"Totaal:             € {totaal} ")
-
-# --- stap 4:
-
-# input in stap 1 ^^ 
 
