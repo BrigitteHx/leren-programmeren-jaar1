f318838695d8cec0629a3f4b3e5ae4fb009d7229
BrigitteHx <99073629@mydavinci.nl>
2022-11-01 10:29:07 +0100

    M04.D01.O5 - Boodschappenlijstje V2

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/#M04.D01.O5 - Boodschappenlijstje.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/#M04.D01.O5 - Boodschappenlijstje.py
@@ -1,31 +1,38 @@
 #M04.D01.O5 - Boodschappenlijstje
 
-vraag = str(input("Wilt u iets aan uw boodschappenlijst toevoegen? "))
-if vraag == "ja":
-    itemslijst = input("Typ wat u toe wilt voegen hier in, vergeet niet om spaties toe te voegen: ")
-    boodschappenlijst = itemslijst.split(" ")
-    print("\n")
-    print("Uw boodschappen lijst: ")
-    print("\n")
-    for naamitem in boodschappenlijst:
-        print(naamitem)
-    print("\n")
-    print("Nog een fijne dag verder!")
-else: 
-    print("Oke, fijne dag verder. ")
 
-# eerste poging, niet uitgekomen: 
+# -----------------------------------------------------------------------------------
 
-# lijst = [] 
+# vraag = str(input("Wilt u iets aan uw boodschappenlijst toevoegen? "))
+# if vraag == "ja":
+#     itemslijst = input("Typ wat u toe wilt voegen hier in, vergeet niet om spaties toe te voegen: ")
+#     boodschappenlijst = itemslijst.split(" ")
+#     print("\n")
+#     print("Uw boodschappen lijst: ")
+#     print("\n")
+#     for naamitem in boodschappenlijst:
+#         print(naamitem)
+#     print("\n")
+#     print("Nog een fijne dag verder!")
+# else: 
+#     print("Oke, fijne dag verder. ")
 
-# while True: 
+# ----------------------------------------------------------------------------------- 
 
+lijst = [] 
+amount = 1
 
-        # boodschappen = item.split(" ")
-        # lijst.append(item)
-    #     # boodschappen = item.split("\n")
-    # elif vraag == "nee":
-    #     # for boodschap in :
-    #     print("Uw boodschappen lijst:")
-    #     print(lijst)
-    #     break 
+while True: 
+
+ vraag = str(input("Wilt u iets aan uw boodschappenlijst toevoegen? "))
+ vraag.lower()
+ if vraag == "ja": 
+    item = input("Wat wilt u toe voegen aan uw boodschappenlijst? ")
+    if item == item:
+        amount =+ 1
+    lijst.append(item)
+ elif vraag == "nee":
+    print("Uw boodschappen lijst:")
+    for x in lijst: 
+        print(amount, x)
+    break   
