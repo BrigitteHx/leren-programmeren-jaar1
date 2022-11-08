2b53a8981cede4aeb8944b382f5236ec65405cd8
BrigitteHx <99073629@mydavinci.nl>
2022-11-08 11:45:05 +0100

    update uitleg opdracht 3 fruitmand

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/Fruitmand/fuitmandopdracht04.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/Fruitmand/fuitmandopdracht04.py
@@ -6,8 +6,8 @@ import random
 hoeveelheid = int(input("Voer hier een aantal in: "))
 random_fruit = random.randint(0,7)
 
-for i in range (0,hoeveelheid):
-    print(fruitmand[random_fruit]['name'])
+for i in range (0,hoeveelheid):                                     # range zodat aangegeven hoeveelheid kan printen 
+    print(fruitmand[random_fruit]['name'])                          # random fruit als variabele ipv [0] zoals in oude code
 
 # --------------------------------------------------------          oude code
 
