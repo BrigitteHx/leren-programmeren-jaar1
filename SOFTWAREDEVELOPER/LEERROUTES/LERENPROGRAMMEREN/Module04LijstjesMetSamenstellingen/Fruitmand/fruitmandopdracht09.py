7631322e7b0af3820d302300d589f846ab3cad72
BrigitteHx <99073629@mydavinci.nl>
2022-11-10 14:49:27 +0100

    M04.D02.O9 - Unieke kleuren zonder de druif

---
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/Fruitmand/fruitmandopdracht09.py
@@ -0,0 +1,15 @@
+# fruitmandopdracht09.py
+
+from fruitmand import fruitmand
+
+for i in range(len(fruitmand)):
+    if fruitmand[i]['name'] == 'druif':
+        fruitmand.pop(i)
+        break
+
+kleuren = []
+
+for fruit in fruitmand:
+    if (fruit['color']) not in kleuren:
+        kleuren.append(fruit['color'])
+print(kleuren)
