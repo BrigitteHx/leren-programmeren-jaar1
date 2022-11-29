74813d2c8d2795f33d22bb70c80a84fe67908d57
BrigitteHx <99073629@mydavinci.nl>
2022-11-29 12:05:27 +0100

    M05.D01.O2 - Hello from function towns!?

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/M05.D01.O1 - Hello from function town.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/M05.D01.O1 - Hello from function town.py
@@ -1,6 +1,6 @@
 # M05.D01.O1 - Hello from function town
 
-def function1():
-  print("Hello from function town")
-  
-function1()
+def function1(nummer):
+    for i in range(nummer):
+        print(f"Hello from function town {i+1}")
+function1(nummer = int(input("Hoeveel keer wilt u het printen? ")))
