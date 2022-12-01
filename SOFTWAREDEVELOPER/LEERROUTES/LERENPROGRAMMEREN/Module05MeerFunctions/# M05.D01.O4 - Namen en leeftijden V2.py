e16e1f38462163b44f48aec338be2ec616059a5f
BrigitteHx <99073629@mydavinci.nl>
2022-12-01 11:28:59 +0100

    M05.D01.O4 - Namen en leeftijden

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/# M05.D01.O4 - Namen en leeftijden V2.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/# M05.D01.O4 - Namen en leeftijden V2.py
@@ -5,11 +5,11 @@ namen_lijst, leeftijd_lijst = [], []
 def namen_leeftijden_lijst():
     start = True
     while start:
-        start = input("Wilt U de lijst maken? ").lower()
+        start = input("Wilt U de (verder) lijst maken? ").lower()
         
         if start == "ja":
-            naam = input("Voer een naam in en enter. ")
-            leeftijd = input("Voer de bijbehorende leeftijd in en klik enter. ")
+            naam = input("Voer een naam in en klik op enter. ")
+            leeftijd = input("Voer de bijbehorende leeftijd in en klik op enter. ")
         
             leeftijd_lijst.append(leeftijd)
             namen_lijst.append(naam)
