fa8682bd53618798f8caabf7da293ab8cf51c6b0
BrigitteHx <99073629@mydavinci.nl>
2022-12-21 14:09:39 +0100

    update game

---
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/game update 2/#story line.py
@@ -0,0 +1,40 @@
+#story line
+
+
+scenes = [
+    {'key' : 'start',
+    'title' : 'start scene',
+    'story' : 'Alright then, youre standing in front of the maze and you can see the 3 ways right in front of you..',
+    'options' : [ 
+        {'key' : 'a', 'next' : 'corner' ,'story' : 'A. What might be laying around that corner? Im going right! ' },
+        {'key' : 'b', 'next' : 'forward', 'story' : 'B. Id like to see more of the Maze, Im going straight forward. '},
+        {'key' : 'c', 'next' : 'left', 'story': 'C. But what about left? What will I find there? Im going to find that out. '}
+    ]
+  },
+  {'key' : 'corner' or 'left',
+  'title' : 'corner scene',
+  'story' : 'On the way you come across another intersection..',
+  'options' : [
+        {'key' : 'a', 'next' : 'route', 'story' : 'A. You choose to ignore the other ways and follow the route, lets go!' },
+        {'key' : 'b', 'next' : 'leftleft', 'story' : 'B. Youre going left, what will that way bring you to?'},
+        {'key' : 'c', 'next' : 'bush', 'story' : 'C. That one bush doesnt look so stable, youre not liking this maze very much and decide to climb over it..'}
+  ]
+  }
+  {'key' : 'forward', 
+  'title' : 'you went forward', 
+  'story' : 'So youve decided to keep going?\n Thats great, while youre walking forward you notice something in the distance.\n Coming closer you realise its an apple tree!\n Those apples look really good, would you like to take a break and eat some? (y/n)' 
+  'options' : [ 
+        {'key' : 'y', 'next' : 'yesapple', 'story' : 'Y. '},
+        {'key' : 'n', 'next' : 'noapple', 'story' : 'N. '}
+
+  ]
+  }
+  {'key' : 'yesapple', 
+  'title' : 'you ate the apple', 
+  'story' : 'Didnt I mention that you should ever trust a maze?!\n The apples end up being poisened and you die.\n Congratulations, you unlocked the BAD ending.\n  Not that you should be proud of it.. Better luck next time!'
+  }
+  {'key': 'noapple',
+  'title' : 'you didnt eat the apple',
+  'story' : 'Nice decision to see through that, those apples were poisened!\n You kept walking and walked straight into victory\n Good job, you found the exit and unlocked the GOOD ENDING!'
+  }
+]
