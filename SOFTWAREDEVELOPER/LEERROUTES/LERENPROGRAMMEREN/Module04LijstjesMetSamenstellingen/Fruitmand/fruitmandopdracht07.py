532a9e67e148dfc90ed98d8b6f3885352de6d88e
BrigitteHx <99073629@mydavinci.nl>
2023-01-04 14:44:06 +0100

    more updates

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/Fruitmand/fruitmandopdracht07.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module04LijstjesMetSamenstellingen/Fruitmand/fruitmandopdracht07.py
@@ -7,3 +7,5 @@ from fruitmand import fruitmand
 for fruit in fruitmand:
     if fruit['round'] == True :
         print(fruit['name'])
+
+        
--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/game update 2/# klad game.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/game update 2/# klad game.py
@@ -39,9 +39,43 @@ hello
 ik ben brigitte
 ik ben 17 jaar 
 doei
-
-
-
-
-
 """)
+
+
+
+
+
+
+
+ scenes = [ 
+  { 'key': 'start',
+    'title': 'Start',
+    'story': 'Situation start... what to do?',
+    'options': [
+      {'key': 'c', 'next' : 'cave', 'story' : 'the dark and misty cave'},
+      {'key': 'h', 'next' : 'hut', 'story' : 'the old wooden hut'},
+      {'key': 't', 'next' : 'tree', 'story' : 'the thick old tree'},
+    ]
+  },
+  { 'key': 'cave',
+    'title': 'Cave',
+    'story': 'Situation cave... wat te doen?',
+    'options': [
+      {'key': 'r', 'next' : 'river', 'story' : 'You choose to the river'},
+      {'key': 'b', 'next' : 'bear-nest', 'story' : 'You choose to the bear nest'},
+      {'key': 'l', 'next' : 'lava-chimney', 'story' : 'You choose to the lava chimney'},
+    ]
+  },
+  { 'key': 'hut',
+    'title': 'Hut',
+    'story': 'Situation hut... wat te doen?',
+    'options': [
+      {'key': 'a', 'next' : 'attic', 'story' : 'You choose to the attic'},
+      {'key': 'b', 'next' : 'bath room', 'story' : 'You choose to the bath room nest'},
+      {'key': 'c', 'next' : 'cellar', 'story' : 'You choose to the cellar'},
+    ]
+  },
+
+
+
+
