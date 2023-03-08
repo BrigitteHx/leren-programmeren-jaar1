a8a40a8748058d9bd71cbae24f8672a1c8eda072
BrigitteHx <99073629@mydavinci.nl>
2023-03-08 14:00:07 +0100

    nieuwe versie (die niet werkt :(.  ) O13 treasure hunt + progress oude versie

--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/M05.D02.O1 - De start/opgave13klad.py
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/M05.D02.O1 - De start/opgave13klad.py
@@ -10,34 +10,53 @@ def getEarnigs(profitGold:float, mainCharacter:dict, friends:list, investors:lis
 
     # haal de juiste inhoud op
     adventuringFriends = getAdventuringFriends(friends)
+    interestingInvestors = getInterestingInvestors(investors)
     adventuringInvestors = getAdventuringInvestors(investors)
-    investorsCuts = getInvestorsCuts(profitGold ,getInterestingInvestors(investors))
-    goldCut = round(getAdventurerCut(profitGold,investorsCuts,len([mainCharacter]+adventuringFriends+getAdventuringInvestors(investors))),2)
+    # investorsCuts = getInvestorsCuts(profitGold ,getInterestingInvestors(investors))
+    # goldCut = round(getAdventurerCut(profitGold,investorsCuts,len([mainCharacter]+adventuringFriends+getAdventuringInvestors(investors))),2)
 
-    donateGold = 10
+    people = [mainCharacter] + friends + investors 
+    peopleTotal = [mainCharacter] + adventuringFriends + adventuringInvestors
+    # donateGold = 10
+    donateGold = len(adventuringFriends) * 10
+    adventuresCut = (profitGold - (getInvestorsCuts(profitGold,investors)) ) / len(peopleTotal)
+    gold = 0
+  
 
     # verdeel de uitkomsten
-    for person in people:
+    for person in range(len(people)):
         #code aanvullen
-        startAdventure = getPersonCashInGold(person["cash"])
-        endAdventure =+ startAdventure
-        if person == mainCharacter:
-            endAdventure =+ round((startAdventure+goldCut) + (donateGold*len(adventuringFriends)),2)
-            # eindbedrag = start bedrag + jou deel + donation (x het aantal vriended)
-        if person in adventuringFriends:
-            endAdventure =+ round((startAdventure+goldCut)-donateGold,2)
-            # eindbedrag = startbedrag + jou deel - 1x donation (want niet hoofd persoon), vrienden dus geen inversteer deel terug
-        if "profitReturn" in person:
-            if person in getInterestingInvestors(investors):
-                # investors die mee gaan:
-                if person in getAdventuringPeople(investors):
-                    endAdventure =+ round((profitGold/100)*person["profitReturn"]+startAdventure+goldCut,2)
-                    # eindbedrag = profit / 100 (want%) x investors profitreturn + start + goldcut 
-                else:
-                    endAdventure =+ round((profitGold/100)*person["profitReturn"]+startAdventure,2)
-                    # eindbedrag = profit / 100 (want%) x investors profitreturn + start (geen goldcut want ging niet mee)
+
+        startAdventure = round(getPersonCashInGold(people[person]['cash']),2)
+        endAdventure = startAdventure
+
+        # main character
+        if people[person] in [mainCharacter]:
+            endAdventure += donateGold + adventuresCut
+        # investors 
+        elif people[person] in [investors]:
+            # investors die mee gingen
+            if people[person] in interestingInvestors and people[person] in adventuringInvestors:
+                gold = round(profitGold / 100 * people[person][ 'profitReturn'],2)
+                endAdventure += gold + adventuresCut
+            # investors die niet mee gingen
+            elif people[person] in interestingInvestors: # deze mist bij andere methode
+                gold = round(profitGold / 100 * people[person][ 'profitReturn'],2)
+                endAdventure += gold
+        # friends die meegingen
+        elif people[person] in [friends]:
+            if people[person] in adventuringFriends:
+                endAdventure += adventuresCut - 10
+        
         earnings.append({
-            'name'   : person["name"],
-            'start'  : startAdventure,
-            'end'    : endAdventure
-        })
+                'name'   : people[person]['name'],
+                'start'  : round(startAdventure),
+                'end'    : round(endAdventure)
+            })      
+
+
+            
+# volgens mij je code is helemaal goed, de enige die misschien fout ging is dat je bij de earnings moet ook mensen die niet mee hebben gedaan in de reis hebben en hun end cash is dus hetzelfd als hun start cash
+# dus die zijn investerso die vroegen meer dan 10% of friends die niet sharewith en adventuring op true hebben
+# dus bij de earnings moeten alle namen van friends en investors en dus niet allen interesting investors en adventuring friends
+# ik weet het niet of mij  uitleg duidelijk was, maar als niet kan ik ook een stukje code laten zien over wat ik bedoel
