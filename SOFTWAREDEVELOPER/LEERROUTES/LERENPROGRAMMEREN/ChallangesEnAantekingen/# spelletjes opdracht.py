263e8fd17e5c1d3fc78c730198fb5c7488c1a462
BrigitteHx <99073629@mydavinci.nl>
2023-03-08 10:57:59 +0100

    planning

---
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/ChallangesEnAantekingen/# spelletjes opdracht.py
@@ -0,0 +1,20 @@
+# spelletjes opdracht 
+
+# PRIJS_SPEL = 24.95
+# KORTING = 0.4
+# spellenVerkocht = 0
+# totaalPrijs = 0 
+
+# aantalSpellen = int(input("Hoeveel spellen? "))
+
+# # prijs met korting berekenen:
+# totaalKorting = PRIJS_SPEL * KORTING
+# prijsMetKorting = PRIJS_SPEL - totaalKorting
+ 
+# totaalEerstePrijs = prijsMetKorting + 1
+# totaalRestSpellenVerzendKosten = ((aantalSpellen - 1) * 0,25)
+# totaalRestSpellen = ((aantalSpellen - 1) * prijsMetKorting )
+# totaalInkoopPrijs = totaalEerstePrijs + totaalRestSpellenVerzendKosten + totaalRestSpellen
+
+# print(totaalInkoopPrijs)
+
