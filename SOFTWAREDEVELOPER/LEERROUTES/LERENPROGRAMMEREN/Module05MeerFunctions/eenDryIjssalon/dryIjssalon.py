db6d9ab1b2839cbeaf9614a5226c950981c5843b
BrigitteHx <99073629@mydavinci.nl>
2023-03-10 13:24:20 +0100

    progress leren programmeren

---
+++ SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/eenDryIjssalon/dryIjssalon.py
@@ -0,0 +1,2 @@
+#  Een Dry Ijssalon
+
--- SOFTWAREDEVELOPER/LEERROUTES/LERENPROGRAMMEREN/Module05MeerFunctions/overige opdrachten/multiplier-x.py
+++
@@ -1,9 +0,0 @@
-#multiplier-x.py
-
-def multiplier(tafel):
-    result = ""
-    for i in range(1,11):
-        result += f"{i} x {tafel} = {i*tafel}\n"
-    return result
-
-print(multiplier(tafel = int(input("Kies een tafel van 1-10. "))))
